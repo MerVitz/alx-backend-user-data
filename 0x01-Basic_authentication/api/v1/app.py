2cb5578cc5a1c5ba58fa450a12be8f2f25f32b6a
MerVitz <amakaluvitalis202@gmail.com>
2024-12-18 06:42:32 +0000

    Added

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -19,6 +19,7 @@ auth_type = getenv("AUTH_TYPE")
 if auth_type == "auth":
     auth = Auth()
 
+
 @app.before_request
 def before_request():
     """ Handle requests before processing """
@@ -26,7 +27,10 @@ def before_request():
         return None
 
     # List of routes that don't need authentication
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/']
 
     # If the current path is in the excluded paths, bypass auth
     if request.path in excluded_paths:
@@ -39,23 +43,26 @@ def before_request():
         if auth.current_user(request) is None:
             abort(403)  # Forbidden
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler """
     return jsonify({"error": "Not found"}), 404
+
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ Forbidden error handler """
     return jsonify({"error": "Forbidden"}), 403
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-    
