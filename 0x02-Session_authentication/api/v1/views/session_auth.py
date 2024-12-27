9f104e3c094814488e626f796d6140996f700edc
MerVitz <amakaluvitalis202@gmail.com>
2024-12-27 10:48:33 +0300

    Added

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,6 +2,7 @@
 """
 Defines session authentication routes
 """
+import os
 from flask import jsonify, request
 from models.user import User
 from api.v1.app import auth
@@ -13,8 +14,6 @@ def session_login():
     POST /api/v1/auth_session/login
     Handles user login with session authentication
     """
-    from api.v1.views import app_views  # Delay import to avoid circular import
-
     email = request.form.get('email')
     password = request.form.get('password')
 
@@ -31,6 +30,7 @@ def session_login():
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
 
+    # Create a session ID for the user
     session_id = auth.create_session(user.id)
     response = jsonify(user.to_json())
     response.set_cookie(os.getenv('SESSION_NAME'), session_id)
