ca6eafe344fd796336930fc2192d28d459d60f0f
MerVitz <amakaluvitalis202@gmail.com>
2024-12-29 09:17:37 +0300

    ixed

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -8,28 +8,17 @@ from user import User
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
+
 my_db = DB()
 
-# Adding a user to the database
 email = 'test@test.com'
 hashed_password = "hashedPwd"
 
 user = my_db.add_user(email, hashed_password)
-print(f"User ID: {user.id}")  # Expect user ID to be printed
+print(user.id)
 
-# Updating the user's password
 try:
     my_db.update_user(user.id, hashed_password='NewPwd')
     print("Password updated")
-except ValueError as e:
-    print(f"Error: {e}")
-
-# Trying to update with an invalid attribute
-try:
-    my_db.update_user(user.id, non_existent_field="value")
-except ValueError as e:
-    print(f"Error: {e}")  # Should print an error about invalid attribute
-
-# Verify the update was successful
-updated_user = my_db.find_user_by(id=user.id)
-print(f"Updated password: {updated_user.hashed_password}")
+except ValueError:
+    print("Error")
