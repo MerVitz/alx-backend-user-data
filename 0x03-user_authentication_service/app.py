7665526083ee91910f114e21c74835b97f5decf1
MerVitz <amakaluvitalis202@gmail.com>
2024-12-29 12:19:20 +0000

    Pycodestyle

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -6,6 +6,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def welcome():
     """
@@ -15,6 +16,7 @@ def welcome():
         JSON: {"message": "Bienvenue"}
     """
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
